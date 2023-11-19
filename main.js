a26f49fd3d3ad6c360bef5fe80398677e0ef6991
madara1319 <mateusz.bik@op.pl>
2023-11-19 10:51:05 +0100

    Adding element by innerHTML doesnt work as expected

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let section=document.querySelector(".section--js");
 let box=document.querySelector(".container--js");
 let clickButton=document.querySelector(".mainButton--js");
-clickButton.addEventListener("click",()=>{section.innerHTML=box;});;
+clickButton.addEventListener("click",()=>{section.innerHTML+=box;});;
 console.log(box);
