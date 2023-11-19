dcb705115aeecd19e152436503fb3f282fdfdcd4
madara1319 <mateusz.bik@op.pl>
2023-11-19 09:13:49 +0100

    querySelector returns null dont kno why

---
+++ main.js
@@ -0,0 +1,5 @@
+let section=document.querySelector("section--js");
+let box=document.querySelector("container--js");
+let clickButton=document.querySelector("mainButton--js");
+//clickButton.addEventListener("click",()=>{section.innerHTML=box;});;
+console.log(box);
