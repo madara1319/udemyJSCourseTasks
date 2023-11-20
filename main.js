35bb93153ad47ea589c92b575d08e5b55bd60301
madara1319 <mateusz.bik@op.pl>
2023-11-20 17:34:24 +0100

    Still doesnt work the way i want

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let section=document.querySelector(".section--js");
 let box=document.querySelector(".container--js");
+let counter=0;
 let clickButton=document.querySelector(".mainButton--js");
-clickButton.addEventListener("click",()=>{section.innerHTML+=box;});;
+clickButton.addEventListener("click",()=>{
+  counter++;
+  section.innerHTML+=`kutas ${counter} ${box.innerHTML}`;
+  console.log(counter++);
+},(false));
 console.log(box);
