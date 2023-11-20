60dd054b4527bdbb05bf497a8977f3e7db600f86
madara1319 <mateusz.bik@op.pl>
2023-11-20 19:23:14 +0100

    cloneNode and appendChild were the methods i was looking for now i have to style them correctly

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let counter=0;
 let clickButton=document.querySelector(".mainButton--js");
 clickButton.addEventListener("click",()=>{
   counter++;
-  section.innerHTML+=`kutas ${counter} ${box.innerHTML}`;
-  console.log(counter++);
-},(false));
+  let clonedBox=box.cloneNode(true);
+  section.appendChild(clonedBox);
+  console.log(counter);
+});
 console.log(box);
