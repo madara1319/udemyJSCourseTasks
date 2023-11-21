4fe063dd2dddd1f7bc2ed3f86f7b12b52703f8e1
madara1319 <mateusz.bik@op.pl>
2023-11-21 21:31:29 +0100

    Added functionality and styles, might style it better and deconsolelog js

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 let section=document.querySelector(".section--js");
 let box=document.querySelector(".container--js");
 let counter=0;
+let mainBox=document.querySelector(".boxOfBoxes--js");
 let clickButton=document.querySelector(".mainButton--js");
+function hasNumber(classList)
+{
+  let classString=Array.from(classList).join(" ");
+  console.log(typeof(classList));
+  console.log(String(classList));
+  let numberInString =Number(classString.match(/\d+/));
+  if(numberInString%5==0)
+  {
+    console.log(`to jest ${ typeof(numberInString) } ${numberInString%5}`);
+    return true;
+  }
+  else
+  {
+    console.log(`To jest ${ typeof(numberInString)} ${ numberInString%5}`);
+    return false;
+}
+}
 clickButton.addEventListener("click",()=>{
   counter++;
+  //console.log(counter);
   let clonedBox=box.cloneNode(true);
-  section.appendChild(clonedBox);
-  console.log(counter);
+  mainBox.appendChild(clonedBox);
+  clonedBox.classList.add(counter);
+  clonedBox.classList.remove('notVisible');
+  clonedBox.textContent=(`${counter}`);
+
+  if(hasNumber(clonedBox.classList))
+    clonedBox.classList.add('circleElement');
+//console.log(`To jest chuuj ${hasNumber(clonedBox.classList)}`)
 });
-console.log(box);
+
+
+//function squareToCircle(){
+//  if ()
+//
+//}
