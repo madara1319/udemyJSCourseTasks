70606db5c409d778c2964793362c11444486783d
madara1319 <mateusz.bik@op.pl>
2023-12-19 22:47:06 +0100

    Cant comprehend algorithm for now

--- sizeOnScrollControl.js
+++ sizeOnScrollControl.js
@@ -1,9 +1,32 @@
 //window.alert("chuj");
 let number=25;
+const background=document.querySelector("body");
 const square=document.querySelector(".container");
-square.addEventListener("click",()=>{
-  number+=5;
+const numberRegex=/\d+/g;
+const windowHeight=window.innerHeight;
+console.log(windowHeight);
+background.addEventListener("wheel",()=>
+  {
+
+    let style=getComputedStyle(square);
+    let dim=1*style.height.match(numberRegex);
+    console.log(dim);
+
+    for (let i=0; dim<=windowHeight/2; i++)
+    {
+
+       number+=5;
+        square.style.height=`${number}px`;
+      square.style.width=`${number}px`;
+    }
+  if(dim>(windowHeight/2))
+  {
+    number-=5;
   square.style.height=`${number}px`;
   square.style.width=`${number}px`;
-  console.log("test klika");
+  }
+  else if(dim>=0)
+  {
+
+  }
 });
