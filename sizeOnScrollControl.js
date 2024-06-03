de631d156c2ec1ea2ba1b346a9cd9f73a67b0fbc
madara1319 <mateusz.bik@op.pl>
2024-06-03 20:36:49 +0200

    addded my solution to square changing size problem

--- sizeOnScrollControl.js
+++ sizeOnScrollControl.js
@@ -6,51 +6,16 @@ const numberRegex=/\d+/g;
 const windowHeight=window.innerHeight;
 console.log(windowHeight);
 
-function checkScroll(){
-
-let style=getComputedStyle(square);
-const currentHeight=1*style.height.match(numberRegex);
-    if ((currentHeight)>windowHeight/2)
-    {
-      lowerTheSquare();
-    }
-    else if((currentHeight)<windowHeight/2||(currentHeight)===0){
-      enlargeTheSquare();
-    }
-}
-checkScroll();
-
-function lowerTheSquare(){
-
-            background.addEventListener("scroll",()=>
-            {
-              number--;
-              square.style.height=`${number}px`;
-              square.style.width=`${number}px`;
-              console.log("zmniejszanie " + number);
-          if((number)<=0)
-              {
-                background.removeEventListener("scroll",lowerTheSquare);
-                checkScroll();
-              }
-            });
-        }
-
-function enlargeTheSquare(){
-
-  background.addEventListener("scroll",()=>
-    {
-      number++;
-      square.style.height=`${number}px`;
-      square.style.width=`${number}px`;
-
-      console.log("powiekszanie " + number);
-      if((number)>=windowHeight/2)
-      {
-        background.removeEventListener("scroll",enlargeTheSquare);
-        checkScroll();
-      }
-      });
-}
-
-
+  let i=0;
+window.addEventListener("wheel",(event)=>{
+  if(event.deltaY>0){
+    console.log(i+=1);
+    square.style.height=`${i*25}px`;
+    square.style.width=`${i*25}px`;
+  }
+  else{
+    console.log(i-=1);
+    square.style.height=`${i*25}px`;
+    square.style.width=`${i*25}px`;
+  }
+})
