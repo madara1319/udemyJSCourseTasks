1d25f78e6486f8aa41623f9163bb6feb34cfbc5a
madara1319 <mateusz.bik@op.pl>
2024-06-04 21:42:22 +0200

    got 2 methods im kinda okay with how they look

--- sizeOnScrollControl.js
+++ sizeOnScrollControl.js
@@ -1,27 +1,44 @@
 class SizeOnWheel {
-  #i = 0
+  #i = 0;
+  //direction for increasing decreasing variant in sizingToggle
+  #direction=1;
   constructor(element) {
     this.element =element;
-    this.elementStyle=getComputedStyle(this.element);
-    this.initialDim=1*this.elementStyle.height;
-
-    this.init();
+    this.initialDim=parseFloat(getComputedStyle(this.element).height);
+    console.log(this.initialDim);
+    this.initSizingToggle();
   }
 //start method using event listener event wheel launching sizeOnWheel method
-  init() {
-    window.addEventListener('wheel',(event)=>{ this.#sizeOnWheel(event,this.element)})
+  initSizeUpOrDown() {
+    window.addEventListener('wheel',(event)=>{ this.#sizeUpOrDown(event,this.element)})
+  }
+
+  initSizingToggle() {
+    window.addEventListener('wheel',(event)=>{ this.#sizingToggle(event,this.element)})
   }
 //private method changing size of element on event(wheel)
-  #sizeOnWheel = (event,element) => {
+  #sizeUpOrDown = (event,element) => {
     if (event.deltaY > 0) {
       this.#i += 1
     } else {
       this.#i -= 1
     }
-    this.elementStyle.style.height = `${this.initialDim+(this.#i * 25)}px`
-    this.elementStyle.style.width = `${this.initialDim+(this.#i * 25)}px`
+    this.element.style.height = `${this.initialDim+(this.#i * 25)}px`
+    this.element.style.width = `${this.initialDim+(this.#i * 25)}px`
     console.log(element.style.height);
   }
+  #sizingToggle=(event,element)=>{
+    //zwieksza caly czas az jak jest wieksze niz wartosc potem zmneijszy o jeden i znow jest mniejszy i zwiekszy ale tylko o ten jeden
+    if ( this.#direction===1 && parseFloat(this.element.style.height)>=(0.5*window.innerHeight) ) {
+      this.#direction=- 1
+    } else if(this.#direction===-1 && (parseFloat(this.element.style.height)<=0) ) {
+      this.#direction = 1
+    }
+    this.#i+=this.#direction;
+    this.element.style.height = `${this.initialDim+(this.#i * 10)}px`
+    this.element.style.width = `${this.initialDim+(this.#i * 10)}px`
+
+  } 
 }
 const square=document.querySelector('.container')
 new SizeOnWheel(square);
