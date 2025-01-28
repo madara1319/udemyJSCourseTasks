57e573e13ea3ccf47ece61b98cf7f4e5bea409bc
Mateusz bIK <mateusz.bik@op.pl>
2025-01-28 22:35:01 +0100

    trying to comeback to learning path

---
+++ addGrowingLiItems.js
@@ -0,0 +1,23 @@
+let size = 10;
+let orderElement = 1;
+const init = ()=> {
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+    const listElement = ``    
+
+
+    button.textContent="Click Here!";
+    div.append(button);
+    body.append(div);
+}
+
+const createLiElements = ()=>{
+    let i = 0;
+    const listElement = document.querySelectorAll('li');
+
+}
+
+init()
+
+
