1a5e3b4b30a764ca2f746910c883b22469fa927e
Mateusz bIK <mateusz.bik@op.pl>
2025-01-29 22:29:58 +0100

    need to change it a little so it fits course task description

--- addGrowingLiItems.js
+++ addGrowingLiItems.js
@@ -4,17 +4,42 @@ const init = ()=> {
     const body = document.querySelector('body');
     const div = document.createElement('div');
     const button = document.createElement('button');
-    const listElement = ``    
-
-
+    let listUL = document.createElement('ul');
+    for (let i=0; i<=size; i++)
+    {
+        let listLI = document.createElement('li');
+        listLI.textContent=`element ${i}`;
+        console.log(listLI.textContent);
+        listUL.append(listLI);
+    }
     button.textContent="Click Here!";
     div.append(button);
+    div.append(listUL);
     body.append(div);
+    
+    let i=0;
+    button.addEventListener("click",()=>{
+
+        createLiElements(listUL);
+    let listElements = document.querySelectorAll('li');  
+        listElements.forEach((element)=>{
+            element.style.display='flex';
+            i+=1;
+            element.style.fontSize=`${i}px`;
+        });
+    })
+  //  console.log(listElements.length);
 }
 
-const createLiElements = ()=>{
+const createLiElements = (inputList)=>{
     let i = 0;
     const listElement = document.querySelectorAll('li');
+
+    for(let i=0; i<=size; i++){
+        let listLI=document.createElement('li');
+        listLI.textContent=`element ${listElement.length+i}`
+        inputList.append(listLI);
+    }
 
 }
 
