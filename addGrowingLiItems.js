0d3c7d55a9e100b2d765afe4a9e0c07a17b55c29
Mateusz bIK <mateusz.bik@op.pl>
2025-01-31 21:38:01 +0100

    task seems to be done correctly

--- addGrowingLiItems.js
+++ addGrowingLiItems.js
@@ -1,65 +1,44 @@
 let size = 10;
 let orderElement = 1;
+
+//starting main function
 const init = ()=> {
+    //create HTML elements 
     const body = document.querySelector('body');
     const div = document.createElement('div');
     const button = document.createElement('button');
     let listUL = document.createElement('ul');
- //   for (let i=0; i<=size; i++)
- //   {
- //       let listLI = document.createElement('li');
- //       listLI.textContent=`element ${i}`;
- //       console.log(listLI.textContent);
- //       listUL.append(listLI);
- //   }
     button.textContent="Click Here!";
     div.append(button);
     div.append(listUL);
     body.append(div);
-    
-    let i=0;
-    button.addEventListener("click",()=>{
+   
 
+    //add callback function to event listener
+    button.addEventListener("click",()=>{
         createLiElements(listUL);
-    let listElements = document.querySelectorAll('li');  
-//        listElements.forEach((element)=>{
-  //          element.style.display='flex';
-    //        i+=1;
-      //      element.style.fontSize=`${i}px`;
-       // });
     })
-  //  console.log(listElements.length);
 }
 
 const createLiElements = (inputList)=>{
     let i = 0;
     let listElement = document.querySelectorAll('li');
-//    console.log(listElement);
-//    if (listElement.length<=0)
-//    {
-//        console.log('false');
-//    }
-//    else{
-//        console.log(listElement[listElement.length-1].style.fontSize)
-//    }
     for(let i=0; i<=size; i++){
         let listLI=document.createElement('li');
+
+        //for the first loop run when no list Elements created
         if (listElement.length<=0)
         {
-
-        listLI.style.fontSize='10px';
+            listLI.style.fontSize=`${size + i}px`;
         }
-        else {
 
-        listLI.style.fontSize=listElement[listElement.length-1].style.fontSize+1;
+        //size up elements depending on the size of the last element on the list
+        else {
+            listLI.style.fontSize=`${i+parseFloat(listElement[listElement.length-1].style.fontSize)}px`;
         }
         listLI.textContent=`element ${listElement.length+i}`
-
-        //const lastLiElementSize = listElement.length.fontSize;
-       // console.log(listLI.style.fontSize + "test");
         inputList.append(listLI);
     }
-
 }
 
 init()
