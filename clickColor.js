bdde9029ad877311b2cefa9ceb70640d4afc62a2
Mateusz bIK <mateusz.bik@op.pl>
2025-02-25 19:46:08 +0100

    added task with ifelse statemts color change click event

--- clickColor.js
+++ clickColor.js
@@ -1,21 +1,25 @@
 //jesli wartosc dla x (event.clientX) i (event.clientY) jest parzysta
 //(np. 100,122) to kolor czerwony jak nieparzysta to niebieski
 //jak jeden taki jeden taki to zielony
-//
+const div = document.querySelector('div');
 const body=document.querySelector('body');
-//body.style.display='flex';
+div.style.height='100vh';
+div.style.alignContent='center';
+body.style.display='flex';
 //body.style.height='100%';
-//body.style.justifyItems='center';
-//body.style.flexDirection='column';
-//body.style.alignItems='center';
+body.style.justifyItems='center';
+body.style.flexDirection='column';
+body.style.alignItems='center';
 //body.style.backgroundColor='hsl(212, 45%, 89%)';
 
-body.addEventListener("mousemove",(e)=>{
+body.addEventListener("click",(e)=>{
     const xAxisPos=e.clientX;
     const yAxisPos=e.clientY;
     const width=window.innerWidth;
     const height=window.innerHeight;
-    if(!xAxisPos%2 && !yAxisPos%2){
+    div.innerHTML=`x = ${xAxisPos}, y = ${yAxisPos} <br> `;
+    div.innerHTML+=` x = ${parseInt(100*xAxisPos/width)}, y = ${parseInt(100*yAxisPos/height)}`;
+    if(!(xAxisPos%2) && !(yAxisPos%2)){
 
     body.style.backgroundColor=`red`;
     }
