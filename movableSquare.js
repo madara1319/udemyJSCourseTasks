9fc53e7c4cc863bc40d7c4d269def1fb1fdb3484
madara1319 <mateusz.bik@op.pl>
2025-03-12 21:05:35 +0100

    e.preventDefault() fixed movableSquare

--- movableSquare.js
+++ movableSquare.js
@@ -2,19 +2,21 @@ console.log("Hello world!")
 let isDragging = false;
 let offsetX, offsetY
 const div = document.querySelector('div');
-div.style.position='fixed';
-div.style.cursor='grab';
-div.style.transition=`background-color 0.2s`
+div.style.position = 'fixed';
+div.style.cursor = 'grab';
+div.style.transition = `background-color 0.2s`
 div.addEventListener('mousedown', (e) => {
     div.style.backgroundColor = 'magenta';
+    div.style.cursor = 'grabbing';
     isDragging = true
+
+    // Oblicz przesunięcie względem lewego górnego rogu div
     offsetX = e.clientX - div.getBoundingClientRect().left;
     offsetY = e.clientY - div.getBoundingClientRect().top;
-    //    offsetX = e.offsetX;
-    //    offsetY = e.offsetY;
-    //    e.preventDefault()
     document.addEventListener('mousemove', onMouseMove)
     document.addEventListener('mouseup', onMouseUp)
+    // Zapobiega zaznaczaniu tekstu podczas przeciągania
+    e.preventDefault();
 
 })
 
@@ -22,29 +24,22 @@ function onMouseMove(e) {
     if (!isDragging) {
         return;
     }
-    requestAnimationFrame(()=>{
-      div.style.left=`${e.clientX - offsetX}px`  
-      div.style.top=`${e.clientY - offsetY}px`  
+    // Użyj requestAnimationFrame dla płynniejszej animacji
+    requestAnimationFrame(() => {
+        div.style.left = `${e.clientX - offsetX}px`
+        div.style.top = `${e.clientY - offsetY}px`
     })
+    // Zapobiega domyślnym zachowaniom przeglądarki
+    e.preventDefault();
 }
 
-function onMouseUp(){
-    isDragging=false;
-    div.style.backgroundColor='#000';
-    document.removeEventListener("mousemove",onMouseMove)
-    document.removeEventListener("mouseup",onMouseUp)
+function onMouseUp() {
+    isDragging = false;
+    div.style.cursor = 'grab';
+    div.style.backgroundColor = '#000';
+
+    // Usuń nasłuchiwanie zdarzeń, aby uniknąć wycieków pamięci
+    document.removeEventListener("mousemove", onMouseMove)
+    document.removeEventListener("mouseup", onMouseUp)
 }
 
-//document.addEventListener('mousemove', (e) => {
-//    console.log(`e.clientX = ${e.clientX}, e.clientY = ${e.clientY}`)
-//    console.log(`div.getBoundingClientRect().left = ${div.getBoundingClientRect().left}, div.getBoundingClientRect().top = ${div.getBoundingClientRect().top}`)
-//    if (isDragging) {
-//        div.style.left = `${e.clientX - offsetX}px`;
-//        div.style.top = `${e.clientY - offsetY}px`;
-//        e.preventDefault();
-//    }
-//})
-//document.addEventListener('mouseup', () => {
-//    div.style.backgroundColor = '#000'
-//    isDragging = false
-//})
