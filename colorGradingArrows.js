69dd001ee2b12a532c0140986b943253a4f998bc
madara1319 <mateusz.bik@op.pl>
2025-03-13 22:08:35 +0100

    little adjustment but still need to look into it

--- colorGradingArrows.js
+++ colorGradingArrows.js
@@ -5,15 +5,17 @@ body.style.justifyItems='center';
 body.style.flexDirection='column';
 body.style.alignItems='center';
 let i = 0
+const MAX_STEPS=100;
 window.addEventListener('keydown', (e) => {
+    const colorValue = Math.round(255 * (1 - i / MAX_STEPS));
     if (e.key === 'ArrowUp') {
         ++i
         console.log(`test numer: ${i}`);
-        body.style.backgroundColor=`#${i*i}`
+        body.style.backgroundColor=`rgb(${colorValue},${colorValue},${colorValue})`
     }
     if (e.key === 'ArrowDown') {
         i--;
         console.log(`test numer: ${i}`);
-        body.style.backgroundColor=`#${i*i}`
+        body.style.backgroundColor=`rgb(${colorValue},${colorValue},${colorValue})`
     }
 })
