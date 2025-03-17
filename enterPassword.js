0492d1b50133c5df3dd7f7b4dbb494de50e5d51e
madara1319 <mateusz.bik@op.pl>
2025-03-17 20:36:27 +0100

    started enterPassword task

---
+++ enterPassword.js
@@ -0,0 +1,8 @@
+const input=document.getElementById('pass');
+
+//keyup event similar to input
+//change also looks cool
+input.addEventListener('input',(e)=>{
+    console.log(e.target.value);
+})
+
