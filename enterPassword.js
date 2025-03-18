9608c431949f848706eed08cad6b26aad8e8a781
madara1319 <mateusz.bik@op.pl>
2025-03-18 21:46:11 +0100

    minor change

--- enterPassword.js
+++ enterPassword.js
@@ -1,8 +1,10 @@
 const input=document.getElementById('pass');
-
+const div=document.querySelector('.message')
 //keyup event similar to input
 //change also looks cool
+
 input.addEventListener('input',(e)=>{
     console.log(e.target.value);
+    div.textContent=e.target.value;
 })
 
