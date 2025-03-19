676e3041579ff679b328f932f2082bb66f83a67a
madara1319 <mateusz.bik@op.pl>
2025-03-19 22:12:24 +0100

    added lsiteners

--- enterPassword.js
+++ enterPassword.js
@@ -2,9 +2,23 @@ const input=document.getElementById('pass');
 const div=document.querySelector('.message')
 //keyup event similar to input
 //change also looks cool
-
+const password="user";
+const message="testMessage"
 input.addEventListener('input',(e)=>{
-    console.log(e.target.value);
-    div.textContent=e.target.value;
+    if(password==e.target.value){
+        div.textContent=message;
+        e.target.value='';
+    }
+    else{
+        div.textContent=''
+    }
 })
 
+input.addEventListener('focus',(e)=>{
+//could use toggle method for classList
+   e.target.classList.add('active') 
+})
+
+input.addEventListener('blur',(e)=>{
+   e.target.classList.remove('active') 
+})
