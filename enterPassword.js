a93a8cc27f776f1690d2d1597e7edb2c14465e0a
madara1319 <mateusz.bik@op.pl>
2025-04-03 21:13:49 +0200

    added arrays for passwords & messages used index in ForEach

--- enterPassword.js
+++ enterPassword.js
@@ -1,28 +1,46 @@
-const input=document.getElementById('pass');
-const div=document.querySelector('.message')
+const input = document.getElementById('pass');
+const div = document.querySelector('.message')
 //keyup event similar to input
 //change also looks cool
-const password="user";
-const message="testMessage"
+const password = "user";
+const message = "testMessage"
 //arrow func doesnt create this
+const messages = [message, "Wiosna to piękna pora roku", "chce się zajebać"]
+const passwords = [password, "wiosna", "mateusz"];
 
-input.addEventListener('input',(e)=>{
-    if(password==e.target.value){
-        div.textContent=message;
-        console.log(e.target.value)
-        e.target.value='';
-    }
-    else{
-        div.textContent=''
-        console.log(div.textContent)
-    }
+input.addEventListener('input', (e) => {
+    div.textContent='';
+    const text = e.target.value;
+    passwords.forEach((element,index) => {
+        if (element === text) {
+            //div.textContent = messages[Math.floor(Math.random() * (messages.length - 1))];
+            div.textContent=messages[index]
+            e.target.value = '';
+        }
+  //      else {
+  //          div.textContent = '';
+
+  //      }
+    })
 })
 
-input.addEventListener('focus',(e)=>{
-//could use toggle method for classList
-   e.target.classList.add('active') 
+
+input.addEventListener('focus', (e) => {
+    //could use toggle method for classList
+    e.target.classList.add('active')
 })
 
-input.addEventListener('blur',(e)=>{
-   e.target.classList.remove('active') 
+input.addEventListener('blur', (e) => {
+    e.target.classList.remove('active')
 })
+//input.addEventListener('input',(e)=>{
+//    if(password==e.target.value){
+//        div.textContent=message;
+//        console.log(e.target.value)
+//        e.target.value='';
+//    }
+//    else{
+//        div.textContent=''
+//        console.log(div.textContent)
+//    }
+//})
