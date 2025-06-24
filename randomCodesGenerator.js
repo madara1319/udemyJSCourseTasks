9eff8a08dcd8cafb5c057b02d37f222a59cad979
madara1319 <mateusz.bik@op.pl>
2025-06-24 22:25:52 +0200

    trying to get back to some coding

--- randomCodesGenerator.js
+++ randomCodesGenerator.js
@@ -1,7 +1,9 @@
 const codes = document.querySelector('section')
 const button = document.querySelector('button')
+const chars='ABCDEFGHIJKLMNOQPRSTUVXZ0123456789';
 const randomCodesGenerator = () => {
-    let randomCode = Math.random()
+    let randomCode = Math.floor(Math.random()*Math.pow(10,10))
+    //const length=Math.floor(Math.random() * (100-10 + 1))+10;
     codes.textContent=`${randomCode}`
 }
 //comment
