6d86672e96ce982365043426cddd68b1a175bfec
madara1319 <mateusz.bik@op.pl>
2025-06-26 22:26:17 +0200

    minor change

--- randomCodesGenerator.js
+++ randomCodesGenerator.js
@@ -3,6 +3,7 @@ const button = document.querySelector('button')
 const chars='ABCDEFGHIJKLMNOQPRSTUVXZ0123456789';
 const randomCodesGenerator = () => {
     let randomCode = Math.floor(Math.random()*Math.pow(10,10))
+    let character=chars[randomCode]
     //const length=Math.floor(Math.random() * (100-10 + 1))+10;
     codes.textContent=`${randomCode}`
 }
