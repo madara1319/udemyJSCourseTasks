e868e4fbda4e05c3fd01ec937b438002e98756c5
madara1319 <mateusz.bik@op.pl>
2025-07-02 22:22:18 +0200

    worked a little on randomcodesgen task

--- randomCodesGenerator.js
+++ randomCodesGenerator.js
@@ -1,11 +1,24 @@
 const codes = document.querySelector('section')
+const section=document.createElement('section');
 const button = document.querySelector('button')
 const chars='ABCDEFGHIJKLMNOQPRSTUVXZ0123456789';
-const randomCodesGenerator = () => {
-    let randomCode = Math.floor(Math.random()*Math.pow(10,10))
-    let character=chars[randomCode]
-    //const length=Math.floor(Math.random() * (100-10 + 1))+10;
-    codes.textContent=`${randomCode}`
+
+
+function singleCodeGenerator(){
+    let array=[];
+    for (let i=0; i<10; i++){
+        array.push(chars[Math.floor(Math.random()*chars.length)])
+        codes.append(`${array.join()}`,section)
+    }
+    console.log(array)
+    console.log(array.join())
+}
+
+function randomCodesGenerator(){
+    for (let i=0; i<1000; i++){
+
+        singleCodeGenerator();
+    }
 }
 //comment
-button.addEventListener('click', randomCodesGenerator)
+button.addEventListener('click', randomCodesGenerator())
