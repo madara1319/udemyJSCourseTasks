6591a2add9a03cc1e3f17c602151903e1f1c80c9
madara1319 <mateusz.bik@op.pl>
2025-07-03 20:25:25 +0200

    finished random codes generator task

--- randomCodesGenerator.js
+++ randomCodesGenerator.js
@@ -1,24 +1,24 @@
 const codes = document.querySelector('section')
-const section=document.createElement('section');
 const button = document.querySelector('button')
 const chars='ABCDEFGHIJKLMNOQPRSTUVXZ0123456789';
 
 
-function singleCodeGenerator(){
+const singleCodeGenerator=()=>{
+    let section=document.createElement('section');
     let array=[];
     for (let i=0; i<10; i++){
         array.push(chars[Math.floor(Math.random()*chars.length)])
-        codes.append(`${array.join()}`,section)
     }
+    codes.append(`${array.join()}`,section)
     console.log(array)
     console.log(array.join())
 }
 
-function randomCodesGenerator(){
+const randomCodesGenerator=()=>{
     for (let i=0; i<1000; i++){
 
         singleCodeGenerator();
     }
 }
 //comment
-button.addEventListener('click', randomCodesGenerator())
+button.addEventListener('click', randomCodesGenerator)
