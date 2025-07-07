3b6fd877e84a3a0424181298b01709ba9b79d0e6
madara1319 <mateusz.bik@op.pl>
2025-07-07 21:20:27 +0200

    finished task add name to array

--- addNameToArray.js
+++ addNameToArray.js
@@ -1,12 +1,33 @@
 const button = document.querySelector('button')
 const div = document.querySelector('div')
 const input = document.querySelector('input')
+const newSec = document.createElement('section')
+const inputArray = [];
+
+const addElementToArray = (element, array) => {
+    if (array.length === 0) {
+        array.push(element);
+    }
+    else {
+        if (array.includes(element)) {
+            window.alert("enter different elemet")
+        }
+        else {
+            console.log("rozne")
+            array.push(element);
+        }
+    }
+}
+
+
+
 
 const addNameToArray = () => {
-    const newSec = document.createElement('section')
-    newSec.textContent = `${input.value}`
-
-    div.append(newSec);
+    addElementToArray(input.value, inputArray)
+    newSec.textContent = `${inputArray.join(', ')}`
+    if(!div.contains(newSec)){
+        div.append(newSec);
+    }
 }
 //albo <button> nie w form albo preventDefault powoduje
 //ze nie ma przeladowania strony za kazdym wpisaniem inputa
